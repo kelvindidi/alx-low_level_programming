37852782b2f41356242f7ba30ddc9d7f1fae9b4a
kelvin <kelvindark407@gmail.com>
2023-09-08 05:02:23 -0700

    ...

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
-/* this is the main fuction of the program*/
 int main(void)
+/*this is the main function of the program*/
 {
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
