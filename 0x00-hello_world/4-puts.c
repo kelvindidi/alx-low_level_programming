2ff0408c1fc8c02adfc2dc225847ca4f326fccbb
kelvin <kelvindark407@gmail.com>
2023-09-08 04:52:44 -0700

    fixed maor typo

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,8 +2,8 @@
 /*
  *a program that prints a simple text using puts() function
  */
-int main(void) 
+int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
-return(0);
+return (0);
 }
