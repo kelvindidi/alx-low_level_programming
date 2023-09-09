ae349db93a967daa7f492b26c830bdc631929cc1
kelvin <kelvindark407@gmail.com>
2023-09-08 21:33:10 -0700

    ..

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -8,9 +8,9 @@ int main(void)
 	char lowerCase;
 	char upperCase;
 
-	for (lowerCase = 'a'; lowerCase < 'z'; lowerCase++)
+	for (lowerCase = 'a'; lowerCase <= 'z'; lowerCase++)
 		putchar(lowerCase);
-	for (upperCase = 'A'; upperCase < 'Z'; upperCase++)
+	for (upperCase = 'A'; upperCase <= 'Z'; upperCase++)
 		putchar(upperCase);
 	putchar(10);
 	return (0);
---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/*
+* mian - this is the main function
+* Returna:0
+*/
+int main(void)
+{
+	char lowerCase;
+	char upperCase;
+	lowerCase = 'a';
+	upperCase = 'A';
+	while (lowerCase < 'z')
+	{
+		if (lowerCase == 'q' || lowerCase == 'e'))
+		{
+			continue;
+		}
+		putchar(lowerCase);
