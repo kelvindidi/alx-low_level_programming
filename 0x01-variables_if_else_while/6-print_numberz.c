778d9b19dbb40a3ae5c049f0a6e9ce4a524b916e
kelvin <kelvindark407@gmail.com>
2023-09-09 00:13:51 -0700

    added 6-print_num... for printing single decimal values with putchar

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+* main - this is the main funtion
+* Return: 0
+*/
+int main(void)
+{
+	int a;
+
+	a = 48;
+	while (a <= 57)
+	{
+		putchar(a);
+		a++;
+	}
+	putchar(10);
+	return (0);
+}
