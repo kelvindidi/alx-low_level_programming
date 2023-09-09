4e6f2bf0747876dd4029b7059f2df97f7a29ed00
kelvin <kelvindark407@gmail.com>
2023-09-08 20:44:06 -0700

    removed newline

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -11,7 +11,7 @@ int main(void)
 	for (i =  0; i < 26; i++)
 	{
 		putchar(alphabets[i]);
-		putchar(10);
+		/*putchar(10);*/
 	}
 	return (0);
 }
---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+/**
+* main - this is where the main function begins
+* Return:0
+*/
+int main(void)
+{
+	
+}
