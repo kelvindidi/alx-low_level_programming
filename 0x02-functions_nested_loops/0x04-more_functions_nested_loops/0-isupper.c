df3c9dab33a68d720d1cf0c4e225046cf376963b
kelvin <kelvindark407@gmail.com>
2023-09-14 06:28:24 -0700

    ..

---
+++ 0x02-functions_nested_loops/0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,9 @@
+#include <main.h>
+/**
+* _isupper - this checks if character is uppercase
+* Return: 1
+*/
+int _isupper(int c)
+{
+	return (isupper(c));
+}
---
+++ 0x02-functions_nested_loops/0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,6 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/* Function declaration */
+int _isupper(int c);
+#endif
