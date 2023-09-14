bb0d8b353d8c1bf0499e7e0ff67a765d6c56aa1d
kelvin <kelvindark407@gmail.com>
2023-09-13 20:22:05 -0700

    ..

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+* print_alphabet - this is the main function
+* Return: void
+*/
+void print_alphabet(void)
+{
+	int a;
+	while (i < 10)
+	{
+		for (a = 'a'; a <= 'z'; a++)
+		{
+		        _putchar(a);
+		}
+	}
+	 _putchar('\n');
+}
