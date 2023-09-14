b52fed13814bd002b361c6a7508acd624a55cddb
kelvin <kelvindark407@gmail.com>
2023-09-14 15:44:36 -0700

    fixed logical errors in 4-print...

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -4,12 +4,14 @@
 */
 void print_most_numbers(void)
 {
-	int num;
+	char num;
 
 	for (num = '0'; num <= '9'; num++)
 	{
-		if (num == 2 || num == 4)
+		if (num == '2' || num == '4')
+		{
 			continue;
+		}
 		_putchar(num);
 	}
 	_putchar('\n');
