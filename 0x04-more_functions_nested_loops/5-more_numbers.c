7dc7d25ab0666250948be88f38f38db26b2ddd4f
kelvin <kelvindark407@gmail.com>
2023-09-14 16:47:09 -0700

    fixed betty errors

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -2,22 +2,22 @@
 
 /**
  * more_numbers - prints numbers from 0 to 14, repeated 10 times,
- *  followed by a new line.
+ * followed by a new line.
  */
 void more_numbers(void)
 {
-        int i, j;
+	int i, j;
 
-        for (i = 0; i < 10; i++)
-        {
-                for (j = 0; j <= 14; j++)
-                {
-                        if (j > 9)
-                        {
-                                _putchar('0' + j / 10);
-                        }
-                        _putchar('0' + j % 10);
-                }
-                _putchar('\n');
-        }
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j <= 14; j++)
+		{
+			if (j > 9)
+			{
+				_putchar('0' + j / 10);
+			}
+			_putchar('0' + j % 10);
+		}
+		_putchar('\n');
+	}
 }
