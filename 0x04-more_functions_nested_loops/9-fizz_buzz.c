04ae431ac84e70d4a97a0b849e20bce025adcbc1
kelvin <kelvindark407@gmail.com>
2023-09-14 18:44:43 -0700

    finished editing fizz buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -2,36 +2,27 @@
 
 int main(void)
 {
-	int i;
+    for (int i = 1; i <= 100; i++)
+    {
+        if (i % 3 == 0 && i % 5 == 0)
+        {
+            printf("FizzBuzz ");
+        }
+        else if (i % 3 == 0)
+        {
+            printf("Fizz ");
+        }
+        else if (i % 5 == 0)
+        {
+            printf("Buzz ");
+        }
+        else
+        {
+            printf("%d ", i);
+        }
+    }
 
-	for (i = 1; i <= 100; i++)
-	{
-		if (i % 3 == 0 && i % 5 == 0)
-		{
-			printf("FizzBuzz");
-		}
-		else if (i % 3 == 0)
-		{
-			printf("Fizz");
-		}
-		else if (i % 5 == 0)
-		{
-			printf("Buzz");
-		}
-		else
-		{
-			printf("%d", i);
-		}
+    printf("\n");
 
-		if (i < 100)
-		{
-			printf("\t");
-		}
-		else
-		{
-			printf("\n");
-		}
-	}
-
-	return (0);
+    return (0);
 }
