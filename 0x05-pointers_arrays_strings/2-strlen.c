584c539c1a1097d0b5969a3eaa8ad683889dfff0
kelvin <kelvindark407@gmail.com>
2023-09-18 07:27:24 -0700

    updated 2-strlen.c -- fixed little issues

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <string.h>
+
+/**
+* _strlen - returns the lenght of a string
+* @s: string to be checked
+* Return: lenght of string
+*/
+
+int _strlen(char *s)
+{
+	return (strlen(s));
+}
