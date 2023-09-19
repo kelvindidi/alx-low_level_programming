4155aa2244fe0c1cb22317310ab23975016b1da9
kelvin <kelvindark407@gmail.com>
2023-09-18 18:32:34 -0700

    done with 4-print_rev

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_rev - prints the reverse of a string
+ * @s: string to be reversed
+ */
+
+void print_rev(char *s)
+{
+	int length = strlen(s);
+	int i;
+
+	for (i = length - 1; i >= 0; i--)
+	{
+		putchar(s[i]);
+	}
+	putchar('\n');
+}
