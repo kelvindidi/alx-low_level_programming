9d509434c9fef3c6ddbe7bfe5d323d35a4d8e57c
kelvin <kelvindark407@gmail.com>
2023-09-20 16:48:31 -0700

    fixed little betty errors

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -5,6 +5,8 @@
 * _strncat - concats n number of src to dest
 * @dest: pointer to string that will be appended
 * @src: pointer to string that will receive dest
+* @n: number of src to be appended
+* Return: dest
 */
 
 char *_strncat(char *dest, char *src, int n)
