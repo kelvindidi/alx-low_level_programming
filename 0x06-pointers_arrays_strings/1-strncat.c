f094f2080b542436b06d07a270911c8ae489080d
kelvin <kelvindark407@gmail.com>
2023-09-20 11:10:34 -0700

    added 1-strncat.c

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,14 @@
+#include <string.h>
+#include "main.h"
+
+/**
+* _strncat - concats n number of src to dest
+* @dest: pointer to string that will be appended
+* @src: pointer to string that will receive dest
+*/
+
+char *_strncat(char *dest, char *src, int n)
+{
+	strncat(dest, src, n);
+	return (dest);
+}
