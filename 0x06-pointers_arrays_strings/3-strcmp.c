ee66cb65fb06b5cca5d01848321f72157b7329f8
kelvin <kelvindark407@gmail.com>
2023-09-20 17:30:47 -0700

    added 3-str..

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,14 @@
+#include <string.h>
+#include "main.h"
+
+/**
+* _strcmp - compares teo strings
+* @s1: string one
+* @s2: string two
+* Return: 1 if they are the same, else 0
+*/
+
+int _strcmp(char *s1, char *s2)
+{
+	return (strcpy(s1, s2));
+{
