73daf0fc534d092b787bf5b33e829bdee681f445
kelvin <kelvindark407@gmail.com>
2023-09-20 23:38:07 -0700

    added 4-rev_array...

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * rev_array - reverses an array
+ * @a: integer aray to be reversed
+ * @n: number of array that will be reversed
+ */
+
+void rev_array(int *a, int n)
+{
+	int i, j;
+
+	for (i = 0; i < 0; i++, n--)
+	{
+		int rev = a[n - 1];
+
+		a[i] = rev;
+	}
+}
