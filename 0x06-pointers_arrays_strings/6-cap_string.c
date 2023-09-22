709863ad41b13a9e760f1ec618b87920dbb37265
kelvin <kelvindark407@gmail.com>
2023-09-21 19:23:41 -0700

    ..

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -9,8 +9,9 @@
 char *cap_string(char *str)
 {
 	int capitalizeNext = 1;
+	int i;
 
-	for (int i = 0; str[i] != '\0'; i++)
+	for (i = 0; str[i] != '\0'; i++)
 	{
 		if (isspace(str[i]) || str[i] == ',' ||
 		str[i] == ';' || str[i] == '.' || str[i] == '!' ||
