3297a1c15136b3db65650dc88a599e54cfd0f160
kelvin <kelvindark407@gmail.com>
2023-09-21 17:53:02 -0700

    added 6-cap_string.c

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,36 @@
+#include <ctype.h>
+/**
+* cap_string - capitalize words in string
+* @str: string to be worked on
+* Return: capitalized string
+*/
+
+char *cap_string(char *str)
+{
+	int capitalizeNext = 1;
+
+	for (int i = 0; str[i] != '\0'; i++)
+	{
+		if (isspace(str[i]) || str[i] == ',' ||
+		str[i] == ';' || str[i] == '.' || str[i] == '!' ||
+		str[i] == '?' || str[i] == '"' || str[i] == '(' ||
+		str[i] == ')' || str[i] == '{' || str[i] == '}')
+		{
+			capitalizeNext = 1;
+		}
+		else
+		{
+			if (capitalizeNext)
+			{
+				str[i] = toupper(str[i]);
+				capitalizeNext = 0;
+			}
+			else
+			{
+				str[i] = tolower(str[i]);
+			}
+		}
+	}
+
+	return (str);
+}
