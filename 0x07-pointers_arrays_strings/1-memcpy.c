05a2ebfe0507cea180f7fd2e5b45aebf8cdf4974
kelvin <kelvindark407@gmail.com>
2023-09-26 15:18:34 -0700

    added new file -- 1-memcpy.c

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,21 @@
+#include <stddef.h>
+/**
+* _memcpy - copies memory area
+* @dest: pointer to string to be coopied from
+* @src: string to b copied to
+* @n: number of bytes to be coopied
+* Return: s
+*/
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	char *original_dest = dest;
+
+	for (unsigned int i = 0; i < n; i++)
+	{
+		dest[i] = src[i];
+	}
+
+	return (original_dest);
+}
+
