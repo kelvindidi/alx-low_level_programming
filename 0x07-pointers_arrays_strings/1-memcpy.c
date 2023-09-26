563b4a873b3e87d6cdc655373bc58d962a8ea857
kelvin <kelvindark407@gmail.com>
2023-09-26 15:27:03 -0700

    fixed pedantic error

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -10,8 +10,9 @@
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	char *original_dest = dest;
+	unsigned int i;
 
-	for (unsigned int i = 0; i < n; i++)
+	for (i = 0; i < n; i++)
 	{
 		dest[i] = src[i];
 	}
