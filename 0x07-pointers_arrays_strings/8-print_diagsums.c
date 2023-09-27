5035b3d5d6ca1e1bf953e7d59a7b2b437f6ca64a
kelvin <kelvindark407@gmail.com>
2023-09-26 18:47:02 -0700

    fixed 8-print_diagsums.c

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -7,17 +7,17 @@
  */
 void print_diagsums(int *a, int size)
 {
-    int sum_diag1 = 0;
-    int sum_diag2 = 0;
-    int i;
+	int sum_diag1 = 0;
+	int sum_diag2 = 0;
+	int i;
 
-    for (i = 0; i < size; i++)
-    {
-        sum_diag1 += a[i * size + i];
+	for (i = 0; i < size; i++)
+	{
+		sum_diag1 += a[i * size + i];
 
-        sum_diag2 += a[i * size + (size - 1 - i)];
-    }
+		sum_diag2 += a[i * size + (size - 1 - i)];
+	}
 
-    printf("%d, %d\n", sum_diag1, sum_diag2);
+	printf("%d, %d\n", sum_diag1, sum_diag2);
 }
 
