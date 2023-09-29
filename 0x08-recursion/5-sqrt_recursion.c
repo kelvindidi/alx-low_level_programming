2dee939958f97d68b2f953a19aec844ab7673084
kelvin <kelvindark407@gmail.com>
2023-09-28 18:49:52 -0700

    finally

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,10 +1,27 @@
 /**
+ * find_sqrt - Helper function to find the square root recursively.
+ * @n: The number for which to find the square root.
+ * @guess: The current guess for the square root.
+ *
+ * Return: The natural square root of n.
+ */
+int find_sqrt(int n, int guess)
+{
+	if (guess * guess == n)
+		return (guess);
+
+	if (guess * guess > n)
+		return (-1);
+
+	return (find_sqrt(n, guess + 1));
+}
+/**
  * _sqrt_recursion - Calculates the natural square root of a number.
  * @n: The number for which to find the square root.
  *
- * Return: The natural square root of n, or -1 if n does not have a natural square root.
+ * Return: The natural square root of n,
+ *  or -1 if n does not have a natural square root.
  */
-int find_sqrt(int n, int guess);
 int _sqrt_recursion(int n)
 {
 	if (n < 0)
@@ -16,13 +33,3 @@ int _sqrt_recursion(int n)
 	return (find_sqrt(n, 1));
 }
 
-int find_sqrt(int n, int guess)
-{
-	if (guess * guess == n)
-		return (guess);
-
-	if (guess * guess > n)
-		return (-1);
-
-	return (find_sqrt(n, guess + 1));
-}
