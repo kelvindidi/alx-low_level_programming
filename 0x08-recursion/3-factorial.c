9da46b30fc84991243d390b3d8bcb6dbc3254802
kelvin <kelvindark407@gmail.com>
2023-09-28 17:08:59 -0700

    added factorial.c

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * factorial - prints the factorial of an integer
+ * @n: integer to be worked on
+ * Return: factorial of integer n
+ */
+
+int factorial(int n)
+{
+	if (n == 1 || n == 0)
+	{
+		return (1);
+	}
+	if (n < 0)
+	{
+		return (-1);
+	}
+	return n * factorial(n - 1)
+}
