aba1d1e42fc1af9e8d85168f363ca05c63b418a5
kelvin <kelvindark407@gmail.com>
2023-09-28 17:44:56 -0700

    added 4-pow...

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+* pow_recursion - returns the value of x raised to the power of y
+* @x: integer to be raised to power
+* @y: the power
+*/
+int pow_recursion(int x; int y)
+{
+	if (y == 0)
+	{
+		return (1);
+	}
+	if (y < 0)
+	{
+		return (-1);
+	}
+	return (x * pow_recursion(x, y - 1));
+}
