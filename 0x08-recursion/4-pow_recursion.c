70cf9c18e95b14b5e824afd6c09877b5a67841e3
kelvin <kelvindark407@gmail.com>
2023-09-28 17:54:43 -0700

    fixed betty error

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -4,6 +4,7 @@
 * _pow_recursion - returns the value of x raised to the power of y
 * @x: integer to be raised to power
 * @y: the power
+* Return: value of x raised to the power of y
 */
 int _pow_recursion(int x, int y)
 {
