ff6eaa03610d0e1caf9c8a20a13219898459c2c6
kelvin <kelvindark407@gmail.com>
2023-09-28 18:56:51 -0700

    done adding 6-is_prime_nimber

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,21 @@
+/**
+ * is_prime_number - Check if a number is prime.
+ * @n: The number to check.
+ *
+ * Return: 1 if n is prime, 0 otherwise.
+ */
+int is_prime_number(int n)
+{
+	int i;
+
+	if (n <= 1)
+		return (0);
+
+	for (i = 2; i * i <= n; i++)
+	{
+		if (n % i == 0)
+			return (0);
+	}
+
+	return (1);
+}
