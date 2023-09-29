fa63b731ba51a4c29e3ba325f0a2fd28b513dbfd
kelvin <kelvindark407@gmail.com>
2023-09-28 19:10:13 -0700

    fixed 6-..

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,4 +1,25 @@
 /**
+ * is_prime_recursive - Helper function to check primality recursively.
+ * @n: The number to check for primality.
+ * @divisor: The current divisor to test.
+ *
+ * Return: 1 if n is prime, 0 otherwise.
+ */
+int is_prime_recursive(int n, int divisor)
+{
+	if (divisor == 1)
+		return (1);
+
+	if (n % divisor == 0)
+		return (0);
+
+	return (is_prime_recursive(n, divisor - 1));
+}
+
+
+
+
+/**
  * is_prime_number - Check if a number is prime.
  * @n: The number to check.
  *
@@ -6,16 +27,8 @@
  */
 int is_prime_number(int n)
 {
-	int i;
-
 	if (n <= 1)
 		return (0);
 
-	for (i = 2; i * i <= n; i++)
-	{
-		if (n % i == 0)
-			return (0);
-	}
-
-	return (1);
+	return (is_prime_recursive(n, n - 1));
 }
