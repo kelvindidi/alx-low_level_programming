f7d3aa537695290b2e47372deadc3509283ad8f0
kelvin <kelvindark407@gmail.com>
2023-09-28 18:21:42 -0700

    addef 5-sqrt...

---
+++ 0x08-recursion/_sqrt_recursion.c
@@ -0,0 +1,27 @@
+/**
+ * _sqrt_recursion - Calculates the natural square root of a number.
+ * @n: The number for which to find the square root.
+ *
+ * Return: The natural square root of n, or -1 if n does not have a natural square root.
+ */
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+		return (-1);
+
+	if (n == 0 || n == 1)
+		return (n);
+
+	return (find_sqrt(n, 1));
+}
+
+int find_sqrt(int n, int guess)
+{
+	if (guess * guess == n)
+		return (guess);
+
+	if (guess * guess > n)
+		return (-1);
+
+	return (find_sqrt(n, guess + 1));
+}
