4ca750476222ff1ac812782db11e18c9fd3989ba
kelvin <kelvindark407@gmail.com>
2023-10-03 14:53:07 -0700

    commit for project o | first try | tested | no bugs

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+* main - a program that prints its name, followed by a new line.
+* @argc: takes in the count of the program command line arguments
+* @argv: makes a string array of the program command line arguments
+* Return: Always 0
+*/
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n");
+	return (0);
+}
