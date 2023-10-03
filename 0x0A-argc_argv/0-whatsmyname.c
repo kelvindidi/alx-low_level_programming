1038b250f7fcb1fa64000961bb2fe7a2b0e04880
kelvin <kelvindark407@gmail.com>
2023-10-03 15:27:34 -0700

    sorry found some bugs lol fixed sha

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,14 +1,16 @@
 #include <stdio.h>
-#include "main.h"
 /**
-* main - a program that prints its name, followed by a new line.
-* @argc: takes in the count of the program command line arguments
-* @argv: makes a string array of the program command line arguments
-* Return: Always 0
-*/
+ * main - a program that prints its name, followed by a new line.
+ * @argc: takes in the count of the program command line arguments
+ * @argv: makes a string array of the program command line arguments
+ * Return: Always 0
+ */
 
 int main(int argc, char *argv[])
 {
-	printf("%s\n");
+	if (argc >= 1)
+	{
+		printf("%s\n", argv[0]);
+	}
 	return (0);
 }
