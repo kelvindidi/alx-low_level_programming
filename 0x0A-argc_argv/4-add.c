05c8d054acd62dd3d14e223f1e155be522d216b6
kelvin <kelvindark407@gmail.com>
2023-10-05 20:08:20 -0700

    fixed betty and Wextra Werror ... erors on project no: 4

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <ctype.h>
 
-/***
+/**
  * main - Adds positive numbers.
  * @argc: count args
  * @argv: stores args in string arrays
@@ -11,34 +11,40 @@
  * adds the positive numbers, and prints the result followed by a new line.
  *
  * Return:
- * 0 - If the program successfully adds the positive numbers and prints the result.
- * 1 - If no numbers are passed to the program or if any argument contains non-digit symbols.
+ * 0 - If the program successfully adds the positive numbers and
+ * prints the result.
+ * 1 - If no numbers are passed to the program or if any argument contains
+ * non-digit symbols.
  */
 
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
+
+	int sum, num, i, j;
+
 	if (argc == 1)
 	{
 		printf("0\n");
-		return 0;
+		return (0);
 	}
 
-	int sum = 0;
+	sum = 0;
 
-	for (int i = 1; i < argc; i++)
+	for (i = 1; i < argc; i++)
 	{
 		char *arg = argv[i];
 
-		for (int j = 0; arg[j] != '\0'; j++)
+		for (j = 0; arg[j] != '\0'; j++)
 		{
 			if (!isdigit(arg[j]))
 			{
 				printf("Error\n");
-				return 1;
+				return (1);
 			}
 		}
 
-		int num = atoi(arg);
+		num = atoi(arg);
 		if (num > 0)
 		{
 			sum += num;
@@ -47,5 +53,5 @@ int main(int argc, char *argv[]) {
 
 	printf("%d\n", sum);
 
-	return 0;
+	return (0);
 }
