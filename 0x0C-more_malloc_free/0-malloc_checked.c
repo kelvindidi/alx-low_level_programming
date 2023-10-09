ee0eb3ed0b03edd80e76a6b89111502f36708c1f
kelvin <kelvindark407@gmail.com>
2023-10-08 21:28:02 -0700

    first correction on 0-...c

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,4 +1,3 @@
-#include <stdio.h>
 #include <stdlib.h>
 
 /**
@@ -13,11 +12,7 @@ void *malloc_checked(unsigned int b)
 	void *ptr = malloc(b);
 
 	if (ptr == NULL)
-	{
-		fprintf(stderr, "Memory allocation failed\n");
 		exit(98);
-	}
 
 	return (ptr);
 }
-
