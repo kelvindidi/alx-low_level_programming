f48c84ef9f85d491343f2ec0c147115b08c36828
kelvin <kelvindark407@gmail.com>
2023-10-10 14:43:15 -0700

    added 1-init_dog.c for project no: 1

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,16 @@
+#include "dog.h"
+
+/**
+* init_dog - a function that initialize a variable of typ struct dog
+* @name: element name
+* @age: age
+* @owner: owner
+* @d: pointer to struct variable
+*/
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
