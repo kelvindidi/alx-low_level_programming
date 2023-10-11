040a4bfcdc2f46df10bb21e2073dc8ea1c37c08b
kelvin <kelvindark407@gmail.com>
2023-10-10 23:03:13 -0700

    fixed project no: 3

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -8,13 +8,12 @@
 * @owner: owner element
 * dog_t - Typedef for struct dog
 */
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
-typedef struct dog dog_t;
+} dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
