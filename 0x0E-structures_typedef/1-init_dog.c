323bee870fec084e7082bcff83bff1a527ce9d16
kelvin <kelvindark407@gmail.com>
2023-10-10 19:30:28 -0700

    added checker for null pointer in 1-init_dog.c

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,15 +1,20 @@
 #include "dog.h"
+#include <stdio.h>
 
 /**
-* init_dog - a function that initialize a variable of typ struct dog
-* @name: element name
-* @age: age
-* @owner: owner
-* @d: pointer to struct variable
-*/
+ * init_dog - a function that initialize a variable of typ struct dog
+ * @name: element name
+ * @age: age
+ * @owner: owner
+ * @d: pointer to struct variable
+ */
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
+	if (d == NULL)
+	{
+		return;
+	}
 	d->name = name;
 	d->age = age;
 	d->owner = owner;
