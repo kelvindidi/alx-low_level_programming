09289c09f519af60dd6f6231d2b86b81a922f2c6
kelvin <kelvindark407@gmail.com>
2023-10-12 12:50:12 -0700

    fixed betty errors

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -4,23 +4,24 @@
  * @size: The number of elements in the array
  * @cmp: A pointer to the comparison function
  *
- * Return: Index of the first matching element, or -1 if no match or invalid input
+ * Return: Index of the first matching element, or
+ * -1 if no match or invalid input
  */
 int int_index(int *array, int size, int (*cmp)(int))
 {
 	if (array == NULL || cmp == NULL || size <= 0)
 	{
-		return -1;
+		return (-1);
 	}
 
 	for (int i = 0; i < size; i++)
 	{
 		if (cmp(array[i]) != 0)
 		{
-			return i;
+			return (i);
 		}
 	}
 
-	return -1;
+	return (-1);
 }
 
