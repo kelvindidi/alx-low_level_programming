419f1154fa08997176f172231b114bcc38a262f6
kelvin <kelvindark407@gmail.com>
2023-10-12 11:33:26 -0700

    ..

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -3,7 +3,7 @@
 
 /**
  * print_name - a function that prints a name
-* @name: name to be printed
+ * @name: name to be printed
  * @f: pointer to a function
  */
 void print_name(char *name, void (*f)(char *))
