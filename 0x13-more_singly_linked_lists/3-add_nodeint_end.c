660e8b6a75fdbabc6d4f1d369bf83923747ea468
kelvin <kelvindark407@gmail.com>
2023-10-24 06:46:49 -0700

    betty error fixed

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -1,11 +1,17 @@
 #include "lists.h"
-
+/**
+ * add_nodeint_end - Adds a new node at the end of a listint_t list.
+ * @head: A pointer to a pointer to the head of the linked list.
+ * @n: The value to be added to the new node.
+ *
+ * Return: The address of the new element, or NULL if it fails.
+ */
 listint_t *add_nodeint_end(listint_t **head, const int n)
 {
 	listint_t *new_node;
 	listint_t *current;
 
-	new_node = malloc(sizeof(listint_t)); 
+	new_node = malloc(sizeof(listint_t));
 	if (new_node == NULL)
 		return (NULL);
 
