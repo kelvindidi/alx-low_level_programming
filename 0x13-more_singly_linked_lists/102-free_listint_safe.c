22a48987947221fd075e5a47ed2d7f1d21ec9a53
kelvin <kelvindark407@gmail.com>
2023-10-24 19:43:59 -0700

    fixed betty errors in all files

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,31 +1,30 @@
 #include "lists.h"
-#include <stdlib.h>
 
 /**
- * free_listint_safe - Frees a listint_t linked list.
- * @h: A pointer to the head of the linked list.
+ * free_listint_safe - Frees a listint_t list.
+ * @h: A pointer to a pointer to the head of the linked list.
  *
  * Return: The size of the list that was freed.
  */
 size_t free_listint_safe(listint_t **h)
 {
-	size_t count = 0;
 	listint_t *current = *h;
-	listint_t *next_node;
+	listint_t *next = NULL;
+	size_t size = 0;
 
 	while (current != NULL)
 	{
-		count++;
-		next_node = current->next;
+		next = current->next;
 		free(current);
-		current = next_node;
+		size++;
+		current = next;
 
-		if (next_node >= *h)
+		if (current == *h)
 		{
+			*h = NULL;
 			break;
 		}
 	}
 
-	*h = NULL;
-	return (count);
+	return (size);
 }
